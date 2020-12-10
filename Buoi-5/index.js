f4ffeba64e431f16d89473ac5cc3b1dcfc6b59da
C9hop <cloud99gg@gmail.com>
2020-12-10 19:29:58 +0700

    update

--- Buoi-5/index.js
+++ Buoi-5/index.js
@@ -1,7 +1,18 @@
 import './utils.js'
 import './screens/register.js'
+import './screens/login.js'
 import './components/inputWrapper.js'
 
-document.getElementById('app').innerHTML =`
-    <register-screen></register-screen>
+redirect('register')
+export function redirect(screenName){
+    if(screenName=='register'){
+        document.getElementById('app').innerHTML =`
+            <register-screen></register-screen>
 `
+    }
+    else if(screenName === 'login'){
+        document.getElementById('app').innerHTML =`
+            <login-screen></login-screen>
+`
+    }
+}
--- Buoi-5/screens/register.js
+++ Buoi-5/screens/register.js
@@ -13,13 +13,20 @@ const style =`
         width: 30%;
         background: #fff;
         height: 100vh;
-
+        padding:0px 20px;
     }
     h1{
         text-align: center;
         color: #333
     }
+    button{
+        background: #1565C0;
+        color: #fff;
+        padding:10px 15px;
+        border-radius: 5px;
+    }
 `
+import {redirect} from '../index.js'
 class RegisterScreen extends HTMLElement{
     constructor(){
         super()
@@ -38,28 +45,76 @@ class RegisterScreen extends HTMLElement{
                     <input-wrapper id="email" type="text" placeholder="Email"></input-wrapper>
                     <input-wrapper id="password" type="password" placeholder="Password"></input-wrapper>
                     <input-wrapper id="confirm-password" type="password" placeholder="Confirm password"></input-wrapper>
-                    <button>Sign up</button>
+                    <button>Register</button>
+                    <a id="redirect">Already have an account? login</a>
                 </form>
             </div>
         `
         const registerForm = this._shadowRoot.getElementById('register-form')
-            registerForm.addEventListener('submit',(e)=>{
+            registerForm.addEventListener('submit', async (e)=>{
                 e.preventDefault()
                 const firstName =  this._shadowRoot.getElementById('first-name').Value
                 const lastName =  this._shadowRoot.getElementById('last-name').Value
                 const email =  this._shadowRoot.getElementById('email').Value
                 const password =this._shadowRoot.getElementById('password').Value
-                addDocument()
-                function addDocument(){
-                    const data = {
-                        name: `${firstName +" "+lastName}`,
-                        email: `${email}`,
-                        password: `${password}`
-                    }
-                    firebase.firestore().collection('users').add(data)
+                const confirmpassword =this._shadowRoot.getElementById('confirm-password').Value
+                let  isValid = true
+                if(firstName.trim() === ''){
+                    isValid = false
+                    this._shadowRoot.getElementById('first-name').setAttribute('error', 'Please input first name')
                 }
-                // firstName = ''
+                if(lastName.trim() === ''){
+                    isValid = false
+
+                    this._shadowRoot.getElementById('last-name').setAttribute('error', 'Please input last name')
+                }                
+                if(email.trim() === ''){
+                    isValid = false
+                    this._shadowRoot.getElementById('email').setAttribute('error', 'Please input email')
+                }                
+                if(password.trim() === ''){
+                    isValid = false 
+                    this._shadowRoot.getElementById('password').setAttribute('error', 'Please input password')
+                }
+                if(confirmpassword.trim() === ''){
+                    isValid = false
+                    this._shadowRoot.getElementById('confirm-password').setAttribute('error', 'Please input confirm password')
+                }
+                if(password !== confirmpassword){
+                    isValid = false
+                   this.setError('confirm-password', "password didn't match")
+                }
+                if(!isValid){
+                    return
+                }
+
+                    const user = {
+                        name: firstName +" "+lastName,
+                        email: email,
+                        password: CryptoJS.MD5(password).toString()
+                    }
+                    //neu email da ton tai ro thi tra ra true
+                    const check = await this.checkEmailExist(email)
+                    if(check){
+                        alert('email da ton tai')
+                    }
+                    else{
+                        firebase.firestore().collection('users').add(user)
+                        alert('Dang ki thanh cong')
+                    }
             })
+            this._shadowRoot.getElementById('redirect')
+            .addEventListener('click', ()=>{
+                redirect('login')
+            })
+    }
+    setError(id, message){
+        this._shadowRoot.getElementById(id).setAttribute('error',message)
+    }
+    async checkEmailExist(email){
+        const res = await firebase.firestore().collection('users')
+        .where('email', '==', email).get()
+        return !res.empty
     }
 }
 window.customElements.define('register-screen',RegisterScreen)
